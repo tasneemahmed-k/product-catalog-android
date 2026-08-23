07b447f6f5f64c974f7ad40266a2f9129386e49e
tasneem-ahmed-k <tasneem.ahmed@koinz.app>
2026-08-23 14:12:33 +0300

    feat(chore): Create products activity

---
+++ app/src/main/java/com/example/product_catalog_android/ui/products/ProductsActivity.kt
@@ -0,0 +1,16 @@
+package com.example.product_catalog_android.ui.products
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class ProductsActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+        }
+    }
+}
