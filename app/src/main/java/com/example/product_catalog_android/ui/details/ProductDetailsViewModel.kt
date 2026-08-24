abe4b18a31b3cae3c842022d5ed20d8b03e93772
tasneem-ahmed-k <tasneem.ahmed@koinz.app>
2026-08-24 14:14:08 +0300

    feat(product item): implement product item screen and it's components

---
+++ app/src/main/java/com/example/product_catalog_android/ui/details/ProductDetailsViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.product_catalog_android.ui.details
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.data.repository.ProductRepository
+import com.example.data.result.DataResult
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class ProductDetailsViewModel(
+    private val repository: ProductRepository
+) : ViewModel() {
+
+    private val _uiState =
+        MutableStateFlow<ProductDetailsUiState>(
+            ProductDetailsUiState.Loading
+        )
+
+    val uiState: StateFlow<ProductDetailsUiState> =
+        _uiState.asStateFlow()
+
+    fun loadProduct(productId: Int) {
+
+        if (productId <= 0) {
+            _uiState.value = ProductDetailsUiState.Empty
+            return
+        }
+
+        viewModelScope.launch {
+
+            _uiState.value = ProductDetailsUiState.Loading
+
+            when (val result = repository.getProductById(productId)) {
+
+                is DataResult.Success -> {
+                    _uiState.value =
+                        ProductDetailsUiState.Success(result.data)
+                }
+
+                is DataResult.Error -> {
+                    _uiState.value =
+                        ProductDetailsUiState.Error(
+                            result.exception.message ?: "Something went wrong."
+                        )
+                }
+            }
+
+        }
+    }
+}
